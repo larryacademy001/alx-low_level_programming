a47a987ea73f4b8d0911d2ee4904afa6b1905486
Larry Academy <larryacademy@gmail.com>
2023-07-18 17:32:09 -0700

    some codes

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: character parameter to check
+ *
+ * Return: 1 if letter, 0 else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
