fc558d90b807f21cbcc31b01d2e08c7dccccaf88
Larry Academy <larryacademy@gmail.com>
2023-07-24 19:27:36 -0700

    codes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - unction that prints a string, in reverse,
+ * followed by a new line.
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+	int strLength = 0;
+
+	while (s[i])
+		strLength++;
+
+	int start = 0;
+	int end = strLength - 1;
+
+	while (start < end)
+	{
+		tempStorage = str[start];
+		str[start] = str[end];
+		str[end] = tempStorage;
+		start++;
+		end--;
+	}
+}
