2e477f8ed7d0ffd9e472cb3ad5397f8c1e01ff1f
Larry Academy <larryacademy@gmail.com>
2023-07-27 18:51:27 -0700

    codes

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - transform to leet
+ * @s: char array string type
+ * Return: s transformed
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char subs[] = "aAeEoOtTlL";
+	char le[] = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; subs[j] != '\0'; j++)
+			if (s[i] == subs[j])
+				s[i] = le[j / 2];
+	}
+
+	return (s);
+}
