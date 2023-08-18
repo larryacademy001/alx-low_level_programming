b81234b1dbbbda3a0daed991140bb8856e6ab4c0
Larry Academy <larryacademy@gmail.com>
2023-08-17 17:47:19 -0700

    some codes

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("(nil)");
 
-		if (i != n - 1 && separator != NULL)
+		if (counter != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 
