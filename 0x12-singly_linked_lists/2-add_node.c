896024e4e6ea8a7d3abd7f2b34ccfc0431990118
Larry Academy <larryacademy@gmail.com>
2023-08-30 16:26:02 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,10 +13,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	unsigned int i = 0;
+	unsigned int len = 0;
 
-	while (str[i])
-		i++;
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 
@@ -24,7 +24,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->i = i;
+	new_node->len = len;
 	new_node->next = (*head);
 	(*head) = new_node;
 
