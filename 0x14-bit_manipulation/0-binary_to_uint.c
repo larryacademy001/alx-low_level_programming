484610495b70d80b6d88a86d12e7bd214122a119
Larry Academy <larryacademy@gmail.com>
2023-09-01 00:31:39 -0700

    correct task o

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_con_numnt - function that converts a binary number to an
+ * binary_to_uint - function that converts a binary number to an
  * unsigned int.
  * @b: is pointing to a string of 0 and 1 chars
  * Return: the converted number, or 0
