6aefb0f4c6634baaf06b430baec32e506256ad63
larryacademy001 <larryacademy@gmail.com>
2024-01-19 20:54:08 -0800

    task 0

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,20 @@
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ *
+ * Return: a pointer to the resulting string
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *dest_end = dest;
+
+	while (*dest_end != '\0')
+		dest_end++;
+
+	while (*src != '\0')
+		*dest_end++ = *src++;
+
+	*dest_end = '\0';
+	return (dest);
+}
